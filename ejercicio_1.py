ec547332786d90475af7f9783b71cbe36dfdf24b
Sergio Blanco Ferrandez <sergio.blanco@expleogroup.com>
2022-05-15 18:28:01 +0200

    agrego el ejercicio 1

---
+++ ejercicio_1.py
@@ -0,0 +1,19 @@
+class Alumno:
+
+    def __init__(self, nombre , nota):
+        self.nombre = nombre
+        self.nota = nota
+    
+    def calificacion(self):
+        print(f"El alumno {self.nombre} ha obtenido la calificación de {self.nota}")
+        if self.nota >= 5:
+            print(f"{self. nombre} ha aprobado")
+        else:
+            print(f"{self. nombre} está suspenso")
+
+alumnado = ['Jose', 'Maria', 'Joaquin', 'Nacho']
+notas = [ 9 , 4 , 3, 10]
+
+for i in range(len(alumnado)):
+    x = Alumno(alumnado[i], notas[i])
+    x.calificacion()
